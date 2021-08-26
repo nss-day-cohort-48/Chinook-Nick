77f1dd843683cff0dde55f41c140a157426d69c6
Nickarmani03 <nickarmani03@gmail.com>
2021-08-26 14:22:04 -0500

    first 18

---
+++ 19.top_2009_agent.sql
@@ -0,0 +1,17 @@
+-- Which sales agent made the most in sales in 2009?
+-- HINT: Use the MAX function on a subquery.
+
+SELECT
+MAX(EmployeeTotalSales2009.Total) AS TopSales_2009,
+EmployeeTotalSales2009.FullName
+FROM (SELECT
+        Employee.FirstName || ' ' || Employee.LastName FullName,
+        ROUND(SUM(Invoice.Total), 2) Total
+    FROM Employee
+    JOIN Customer
+        ON Customer.SupportRepId = Employee.EmployeeId
+    JOIN Invoice
+        ON Invoice.CustomerId = Customer.CustomerId
+    WHERE InvoiceDate BETWEEN '2009-01-01' AND '2009-12-31'
+    GROUP BY Employee.EmployeeId
+    ) AS EmployeeTotalSales2009;
