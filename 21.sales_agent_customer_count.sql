3748b14535d95d88068374b93394824c6b464cd4
Nickarmani03 <nickarmani03@gmail.com>
2021-08-27 09:24:29 -0500

    question 21

--- 21.sales_agent_customer_count.sql
+++ 21.sales_agent_customer_count.sql
@@ -1,3 +1,12 @@
 -- Provide a query that shows how many customers are assigned to each employee. The resultant table should include:
 -- Employee full name
 -- Total number of customers assigned to each employee (even if it's zero)
+
+SELECT
+Employee.FirstName || ' ' || Employee.LastName EmployeeName,
+COUNT(Customer.SupportRepId) Customers
+FROM
+Employee
+LEFT JOIN Customer
+    ON Customer.SupportRepId = Employee.EmployeeId
+GROUP BY EmployeeName;
