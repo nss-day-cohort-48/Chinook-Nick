9bbe7327db162abcd25bb358f98f9ae0141d23dc
Nickarmani03 <nickarmani03@gmail.com>
2021-08-27 10:24:08 -0500

    question 24

--- 24.top_2013_track.sql
+++ 24.top_2013_track.sql
@@ -1,1 +1,13 @@
 -- Provide a query that shows the most purchased track(s) of 2013.
+
+SELECT
+Track.Name TrackName,
+COUNT(InvoiceLine.TrackId) TracksSold
+FROM Track
+JOIN InvoiceLine
+    ON InvoiceLine.TrackId = Track.TrackId
+JOIN Invoice
+    ON Invoice.InvoiceId = InvoiceLine.InvoiceId
+WHERE Invoice.InvoiceDate LIKE '%2013%'
+GROUP BY TrackName
+ORDER BY TracksSold DESC;
