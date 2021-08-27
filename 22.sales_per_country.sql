54fc8e4f029a693d32db80a7df50473dd30a2627
Nickarmani03 <nickarmani03@gmail.com>
2021-08-27 10:21:09 -0500

    question 22

--- 22.sales_per_country.sql
+++ 22.sales_per_country.sql
@@ -1,1 +1,8 @@
 -- Provide a query that shows the total sales per country.
+
+SELECT
+Invoice.BillingCountry Country,
+ROUND(SUM(Invoice.Total), 2) TotalSales
+FROM
+Invoice
+GROUP BY BillingCountry;
