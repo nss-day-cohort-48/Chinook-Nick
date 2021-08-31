67d95ab59c8d26bed36a2959719e4a3def20e120
Nickarmani03 <nickarmani03@gmail.com>
2021-08-31 10:49:59 -0500

    completed

--- 25.top_5_tracks.sql
+++ 25.top_5_tracks.sql
@@ -1,1 +1,14 @@
 -- Provide a query that shows the top 5 most purchased tracks over all.
+
+  
+SELECT
+Track.Name Track,
+COUNT(InvoiceLine.TrackId) TracksSold
+FROM Track
+JOIN InvoiceLine
+    ON InvoiceLine.TrackId = Track.TrackId
+JOIN Invoice
+    ON Invoice.InvoiceId = InvoiceLine.InvoiceId
+GROUP BY TrackName
+ORDER BY TracksSold DESC
+LIMIT 5;
--- 26.top_3_artists.sql
+++ 26.top_3_artists.sql
@@ -1,1 +1,17 @@
 -- Provide a query that shows the top 3 best selling artists.
+
+SELECT
+Artist.Name Artist,
+COUNT(InvoiceLine.TrackId) TracksSold
+FROM Artist
+JOIN Album
+    ON Album.ArtistId = Artist.ArtistId
+JOIN Track
+    ON Track.AlbumId = Album.AlbumId
+JOIN InvoiceLine
+    ON InvoiceLine.TrackId = Track.TrackId
+JOIN Invoice
+    ON Invoice.InvoiceId = InvoiceLine.InvoiceId
+GROUP BY Artist
+ORDER BY TracksSold DESC
+LIMIT 3;
